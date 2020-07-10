0829766a0101c07650afdb6b8756b9e31ccc86c4
mdivincenzo88 <mdivincenz@hotmail.com>
2020-07-10 09:29:44 -0500

    Version with homepage

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,7 @@ Rails.application.routes.draw do
   get 'signup', to: 'user#new', as: 'signup'
   post 'signup', to: 'user#create'
   get 'login', to: 'sessions#new', as: 'login'
-  post 'sessions', to: 'session#create', as: 'sessions'
+  post 'sessions', to: 'sessions#create', as: 'sessions'
 
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
